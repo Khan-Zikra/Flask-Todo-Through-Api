a7e9ed6558b4601ac08ec869ccd922a156641817
zikra <zikrakhan1032@gmail.com>
2023-02-14 10:55:50 +0530

    File parsing and uploading

--- application/app.py
+++ application/app.py
@@ -2,6 +2,16 @@ from flask import Flask , request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime
+import xlsxwriter
+import openpyxl
+from openpyxl import Workbook
+
+# file uploading
+import os
+from werkzeug.utils import secure_filename
+from openpyxl import load_workbook
+from flask import send_from_directory
+
 
 
 # Instantiate flask app
@@ -18,24 +28,31 @@ db = SQLAlchemy(app)
 # Create marshmallow object
 ma = Marshmallow(app)
 
+
+# activate Workbook
+wb = Workbook()
+ws = wb.active
+
 # Create database
 class TodoList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200),nullable=False)
-    description = db.Column(db.String(400),nullable=False)
-    completed = db.Column(db.Boolean, nullable = False, default=False)
+    age = db.Column(db.Integer,nullable=False)
+    email = db.Column(db.String(100),nullable=False) 
+    phone = db.Column(db.Integer,nullable=False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    
 
 
 
-    def __repr__(self):
-        return self.id
+    def __repr__(self) -> str:
+        return f"{self.id}- {self.name}"
 
 
 #Create TodoList Schema
 class TodoListSchema(ma.Schema):
     class Meta:
-        fields = ('id','name', 'description', 'completed','date_created')
+        fields = ('id','name', 'age','email','phone','date_created')
 
 
 #Create instance of schemas
@@ -43,14 +60,18 @@ todolist_schema = TodoListSchema(many=False)
 todolists_schema = TodoListSchema(many=True)
 
 
-@app.route("/todolist", methods =["POST"])
-def add_todo():
+@app.route("/fileparsing", methods =["POST"])
+def add_single_todo():
 
     try:
         name = request.json['name']
-        description =request.json['description']
-
-        new_todo = TodoList(name = name ,description = description)
+        age =request.json['age']
+        email =request.json['email']
+        phone =request.json['phone']
+    
+        
+        new_todo = TodoList(name = name , age = age ,email = email , phone = phone)
+      
 
         db.session.add(new_todo)
         db.session.commit()
@@ -59,55 +80,139 @@ def add_todo():
 
     except Exception as e:
         return jsonify({"Error":"Invalid request."})
+    
+# Add Multiple
+@app.route("/fileparse", methods =["POST"])    
+def addmultiple():
+        data = request.get_json()
+        print('data',data)
+    
+        for items in data:
+            name = items["name"]
+            age = items["age"]
+            email = items["email"]
+            phone = items["phone"]
+
+            new_todo = TodoList(name=name , age=age ,email=email , phone=phone)
+            db.session.add(new_todo)
+            db.session.commit()
+            new_todo = TodoList.query.all()
+
+        return todolists_schema.jsonify(new_todo)
+    
+   
+
 
 # Get todos
-@app.route("/todolist", methods=["GET"])
-def get_todos():
+@app.route("/fileparsing", methods=["GET"])
+def get_multiple_todo():
     todos = TodoList.query.all()
-    result_set = todolists_schema.dump(todos)
+    result_set = todolists_schema.dump(todos)   
     return jsonify(result_set)
 
 
 
-@app.route("/todolist/<int:id>", methods=["GET"])
+
+@app.route("/fileparsing/<int:id>", methods=["GET"])
 def get_todo(id):
     todo = TodoList.query.get_or_404(int(id))
     return todolist_schema.jsonify(todo)
 
+@app.route("/excelfile", methods=["GET"])
+def alldata():
+   
+    dic = {}
+    Alldata = TodoList.query.all()
+    for i in Alldata:
+        
+        dic.update({"name":i.name, "age":i.age, "email": i.email, "phone":i.phone, "date_created":i.date_created})
+        
 
-# Update todo
-@app.route("/todolist/<int:id>", methods= ["PUT"])
-def update_todo(id):
-    todo = TodoList.query.get_or_404(int(id))
+    
 
-    name = request.json['name']
-    description =request.json['description']
-    completed = request.json['completed']
+    workbook = xlsxwriter.Workbook("python_toexcel.xlsx")
+    worksheet = workbook.add_worksheet("firstSheet")
 
-    todo.name = name
-    todo.description = description
-    todo.completed = completed
+    worksheet.write('A1' ,"name")   
+    worksheet.write('B1',"age") 
+    worksheet.write('C1',"email")
+    worksheet.write('D1',"phone")
+    worksheet.write('E1',"date_created")
+   
 
-    db.session.commit()
+    for index , entry in enumerate(Alldata):
+   
+        worksheet.write(index+1, 0, entry.name)
+        worksheet.write(index+1, 1, entry.age)
+        worksheet.write(index+1, 2, entry.email)
+        worksheet.write(index+1, 3, entry.phone)
+        ws = entry.date_created.strftime('%x %X')
+        # z= ws.strftime('%x %X')
+        worksheet.write(index+1, 4, ws)
+        
 
-    return todolist_schema.jsonify(todo)
+
+    workbook.close()
+    return jsonify({"mesg":"Done"})
 
 
-# Delete todo
-@app.route("/todolist/<int:id>", methods= ["DELETE"])
+# Delete File Parsing
+@app.route("/fileparsing/<int:id>", methods= ["DELETE"])
 def delete_todo(id):
     todo = TodoList.query.get_or_404(int(id))
     db.session.delete(todo)
     db.session.commit()
-    return jsonify({"Success": "Todo deleted."})
-
-
-    
+    return jsonify({"Success": "Delete"})
      
-# create database
-with app.app_context():
-    db.create_all()
+
+# For upload file
+@app.route("/uploadfile", methods= ["POST"])
+def upload_file():
+    file = request.files["filenames"]
+    print("file..............", file)
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.active
+    rows = sheet.max_row
+    cols = sheet.max_column
+    print(rows, "rooooooooooooows")
+    print(cols,"coluuuuuuuuuuuumn")
+
+    for r in range(1,rows+1):
+        for c in range(1, cols+1):
+            print(sheet.cell(row=r, column=c).value, end = "     ")
+        print()
+
+    lst = []
+    file.save(os.path.join('static', secure_filename(file.filename)))
+    wb= load_workbook(file)
+    sheet = wb.active
+    for row in sheet.iter_rows(min_row=1, min_col=1):
+
+        name = row[0].value
+        age = row[1].value
+        email = row[2].value
+        phone = row[3].value
+        # date_created =row[4].value
+        lst.append([name,age, email,phone])
+    
+    for i in lst[1: ]:
+        val = TodoList(name=i[0],age=i[1],email=i[2],phone=i[3])
+        db.session.add(val)
+    db.session.commit()
+    return jsonify({"message":" Upload File successfully."})
+
+# For Download file
+def download_file(data):
+    return send_from_directory('static',data, as_attachment =True)
+    
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# create database
+with app.app_context():
+    db.create_all()
